5ea9d5c5efe3a253b66db4a9222f270677c4430c
TATIBOUET <darkana.polofowl@gmail.com>
2018-12-07 20:06:03 +0100

    Connexion  à la base de données

---
+++ Projet Air Atlantique/DAL_Model/DbConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Air_Atlantique.DAO_Model
+{
+    class DbConnection
+    {
+        private static MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public Boolean OpenConnection()
+        {
+            server = "localhost";
+            database = "air_atlantique";
+            uid = "admindb";
+            password = "KSKskJNggKWr9EEZ";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return true;
+        }
+    }
+}
