16be666c2cbeda1edb575af821ad75950ed671d4
TATIBOUET <darkana.polofowl@gmail.com>
2018-12-07 20:41:16 +0100

    DAO Aeroport

--- Projet Air Atlantique/DAL_Model/DbConnection.cs
+++ Projet Air Atlantique/DAL_Model/DbConnection.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows;
+using MySql.Data.MySqlClient;
 
 namespace Projet_Air_Atlantique.DAO_Model
 {
--- Projet Air Atlantique/DAO_Model/Aeroport.cs
+++ Projet Air Atlantique/DAO_Model/Aeroport.cs
@@ -8,5 +8,60 @@ namespace Projet_Air_Atlantique
 {
     class Aeroport
     {
+        private int IdAeroport;
+        private string Nom;
+        private string Ville;
+        private string CodeAITA;
+        private int Pays_idPays;
+
+        public int GetIdAeroport()
+        {
+            return this.IdAeroport;
+        }
+
+        public void SetIdAeroport(int IdAeroport)
+        {
+            this.IdAeroport = IdAeroport;
+        }
+
+        public string GetNom()
+        {
+            return this.Nom;
+        }
+
+        public void SetNom(string Nom)
+        {
+            this.Nom = Nom;
+        }
+
+        public string GetVille()
+        {
+            return this.Ville;
+        }
+
+        public void SetVille(string Ville)
+        {
+            this.Ville = Ville;
+        }
+
+        public string GetCodeAITA()
+        {
+            return this.CodeAITA;
+        }
+
+        public void SetCodeAITA(string CodeAITA)
+        {
+            this.CodeAITA = CodeAITA;
+        }
+
+        public int GetPays_idPayst()
+        {
+            return this.Pays_idPays;
+        }
+
+        public void SetPays_idPays(int Pays_idPays)
+        {
+            this.Pays_idPays = Pays_idPays;
+        }
     }
 }
