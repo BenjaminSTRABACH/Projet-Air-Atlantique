14c82169bc39b86d308957054270998747db3dc2
TATIBOUET <darkana.polofowl@gmail.com>
2018-12-08 12:34:31 +0100

    DAO Passager
    Changement Client => Passager

---
+++ Projet Air Atlantique/DAO_Model/Passager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Air_Atlantique
+{
+    class Passager
+    {
+        private int IdPassager;
+        private string Nom;
+        private string Prenom;
+        private int Genre;
+        private DateTime DateNaissance;
+        private int PointsFidelite;
+        private int Tel;
+        private string Mail;
+
+        public int GetIdPassager()
+        {
+            return this.IdPassager;
+        }
+
+        public string GetNom()
+        {
+            return this.Nom;
+        }
+
+        public string GetPrenom()
+        {
+            return this.Prenom;
+        }
+
+        public int GetGenre()
+        {
+            return this.Genre;
+        }
+
+        public DateTime GetDateNaissance()
+        {
+            return this.DateNaissance;
+        }
+
+        public int GetPointsFidelite()
+        {
+            return this.PointsFidelite;
+        }
+
+        public int GetTel()
+        {
+            return this.Tel;
+        }
+
+        public string GetMail()
+        {
+            return this.Mail;
+        }
+
+    }
+}
