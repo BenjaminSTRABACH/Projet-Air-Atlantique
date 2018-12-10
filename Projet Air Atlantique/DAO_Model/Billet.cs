2b682ad8b409eb3e86eb90bf9ca8cdd2649b7067
TATIBOUET <darkana.polofowl@gmail.com>
2018-12-10 11:28:02 +0100

    DAO : Terminée

--- Projet Air Atlantique/DAO_Model/Billet.cs
+++ Projet Air Atlantique/DAO_Model/Billet.cs
@@ -9,61 +9,14 @@ namespace Projet_Air_Atlantique
     class Billet
     {
 
-        private int idBillet;
-        private double prix;
-        private int Passager_idPassager;
-        private int AeroportDepart_idAeroport;
-        private int AeroportArrivee_idAeroport1;
+        private int IdBillet;
+        private double Prix;
 
-
-        public int GetidBillet()
+        public double PrixProperty
         {
-            return this.idBillet;
+            get { return Prix; }
+            set { Prix = value; }
         }
 
-        public void SetidBillet(int idBillet)
-        {
-            this.idBillet = idBillet;
-        }
-
-        public double Getprix()
-        {
-            return this.prix;
-        }
-
-        public void Setprix(double prix)
-        {
-            this.prix = prix;
-        }
-
-        public int GetPassager_idPassager()
-        {
-            return this.Passager_idPassager;
-        }
-            
-        public void SetPassager_idPassager(int Passager_idPassager)
-        {
-            this.Passager_idPassager = Passager_idPassager;
-        }
-
-        public int GetAeroportDepart_idAeroport()
-        {
-            return this.AeroportDepart_idAeroport;
-        }
-
-        public void SetAeroportDepart_idAeroport(int AeroportDepart_idAeroport)
-        {
-            this.AeroportDepart_idAeroport = AeroportDepart_idAeroport;
-        }
-
-        public int GetAeroportArrivee_idAeroport1()
-        {
-            return this.AeroportArrivee_idAeroport1;
-        }
-
-        public void SetAeroportArrivee_idAeroport1(int AeroportArrivee_idAeroport1)
-        {
-            this.AeroportArrivee_idAeroport1 = AeroportArrivee_idAeroport1;
-        }
     }
 }
---
+++ Projet Air Atlantique/DAO_Model/Motorisation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Air_Atlantique.DAO_Model
+{
+    class Motorisation
+    {
+        private int IdMotorisation;
+        private string Libelle;
+
+        public Motorisation() { }
+
+        public Motorisation(int IdMotorisation, string Libelle)
+        {
+            this.IdMotorisation = IdMotorisation;
+            this.Libelle = Libelle;
+        }
+
+        public string LibelleProperty
+        {
+            get { return Libelle; }
+            set { Libelle = value; }
+        }
+    }
+}
